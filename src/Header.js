b3dd56fe8b50759d9c661898e203d5a485012dd6
Victoria-FZ-eng <vzulof@gmail.com>
2021-06-20 15:08:31 +0300

    lab 11

---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
+import './header.css';
+import { withAuth0 } from '@auth0/auth0-react';
+import LoginButton from './LoginButton.js';
+import LogoutButton from './LogoutButton.js';
+
+class Header extends React.Component {
+  
+  render() {
+    return(
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand>My Favorite Books</Navbar.Brand>
+          <Link to="/">Home</Link>
+          <Link to="/profile">Profile</Link>
+          {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+        
+         
+         {/* {!this.props.auth0 && <LoginButton />}
+         {this.props.auth0 && <LogoutButton />} */}
+
+         {this.props.auth0.isAuthenticated ? <LogoutButton/> : <LoginButton />}
+        
+      </Navbar>
+    )
+  }
+}
+
+export default withAuth0(Header);
